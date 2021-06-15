5ec91b25d7a035824fc1d533511bfeb93e6b1c57
dhonadwi <dhonadwi2@gmail.com>
2021-06-15 14:55:53 +0700

    adding websocket

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,9 @@ import '../styles/materialize.min.css';
 import './js/materialize.min.js';
 import '../styles/styles.css';
 import '../styles/responsive.css';
+import WebSocketInitiator from './utils/websocket-initiator';
 import renderPage from './views/app';
+import CONFIG from './global/config';
 
 window.addEventListener('hashchange', () => {
   renderPage();
@@ -15,3 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
   const sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
 });
+
+window.addEventListener('load', () => {
+  document.querySelector('#homeContent').innerHTML += 'halo';
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
+})
